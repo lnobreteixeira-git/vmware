403362c6740f01cebe37881902f81eb4cdf7640f
lnobreteixeira-git <71680589+lnobreteixeira-git@users.noreply.github.com>
2021-11-18 18:10:26 -0300

    Update vmware-provision.ps1

--- vmware-provision.ps1
+++ vmware-provision.ps1
@@ -4,4 +4,4 @@ $vmname = "nome da vm"
 $datastore = "nome do datastore"
 $NetworkName = "nome da rede"
 $Template = Get-Template -Name "meu template"
-New-VM -Name $vmname -Template -ResourcePool $meuhost -Datastore $datastore -NumCpu 2 -MemoryGB 4 -DiskGB 40 -NetworkName $NetworkName -Floppy -CD -DiskStorageFormat Thin -GuestId winNetDatacenterGuest
+New-VM -Name $vmname -Template $template -ResourcePool $meuhost -Datastore $datastore -NumCpu 2 -MemoryGB 4 -DiskGB 40 -NetworkName $NetworkName -Floppy -CD -DiskStorageFormat Thin -GuestId winNetDatacenterGuest
