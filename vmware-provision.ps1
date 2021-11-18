fe5b6e4f891bf6a46ddc8e489f746134a5f5e667
lnobreteixeira-git <71680589+lnobreteixeira-git@users.noreply.github.com>
2021-11-18 16:54:41 -0300

    Add files via upload

---
+++ vmware-provision.ps1
@@ -0,0 +1,7 @@
+﻿connect-viserver vctcor00401p.intraservice.corp -User 'usuario' -Password 'senha' -AllLinked
+$meuhost = Get-VMHost -Name "meuhost"
+$vmname = "nome da vm"
+$datastore = "nome do datastore"
+$NetworkName = "nome da rede"
+$Template = Get-Template -Name "meu template"
+New-VM -Name $vmname -Template -ResourcePool $meuhost -Datastore $datastore -NumCpu 2 -MemoryGB 4 -DiskGB 40 -NetworkName $NetworkName -Floppy -CD -DiskStorageFormat Thin -GuestId winNetDatacenterGuest
